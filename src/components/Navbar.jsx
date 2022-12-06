08c7b49062c7facedc1ed2455762b683e0635f6f
azimjonking <azimjonjalilov1301@mail.ru>
2022-12-06 18:59:51 +0200

    Navbar is started

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Container = styled.div`
+    height: 60px;
+`;
+
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+`;
+
+const Left = styled.div`
+    flex: 1;
+`;
+
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+`
+
+const Center = styled.div`
+    flex: 1;
+`;
+const Right = styled.div`
+    flex: 1;
+`;
+
+const Navbar = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Left>
+                    <Language>EN</Language>
+                </Left>
+                <Center>center</Center>
+                <Right>right</Right>
+            </Wrapper>
+        </Container>
+    )
+}
+
+export default Navbar;
